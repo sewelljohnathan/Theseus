0ff45130c0f6a09912fedcd805f1b21cc66e2141
Johnathan Sewell <sewelljohnathan@proton.me>
2024-07-24 20:12:40 -0400

    Redo parts and effect

--- src/parts.py
+++ src/parts.py
@@ -1,7 +1,12 @@
+"""
+Module contains the Part class.
+"""
+
 import os
 import random
 from collections.abc import Iterable
 
+from effect import Effect, ProportionalEffect, StaticEffect
 from utils import load_jsonc
 
 
@@ -11,30 +16,38 @@ class Part:
 
     NOTE: Construction based on json data assumes a certain structure and a
     json structure that differs may lead to undefined behavior, if not erroring.
-    Ensure that the json document passes this regex:
-    {\s*(?:"[\w\d\s]+":\s*{\s*"baseStats":\s*{\s*"stitch":\s*[0-9]+,\s*"nerve":\s*[0-9]+,\s*"bone":\s*[0-9]+\s*},\s*"sockets":\s*{\s*"female":\s*[0-9]+,\s*"male":\s*[0-9]+\s*},\s*"slots":\s*[0-9]\s*},?\s*)*}
     """
 
     def __init__(
         self,
-        stitches: int,
-        nerves: int,
-        bones: int,
+        health: float,
+        attack: float,
+        defense: float,
         female_sockets_max: int,
         male_sockets_max: int,
         slots_max: int,
         modifier: float,
         titles: tuple[str],
     ):
+        """Instantiate Part."""
         if isinstance(titles, Iterable) is False:
             raise ValueError('Argument "titles" must be an iterable.')
 
-        # Rarity / Minigame modifier
-        stitches *= modifier
-        nerves *= modifier
-        bones *= modifier
+        self.health = health
+        self.health_max = health
+        self.defense = defense
+        self.attack = attack
+        self.female_sockets: list[Part] = []
+        self.female_sockets_max = female_sockets_max
+        self.male_sockets: list[Part] = []
+        self.male_sockets_max = male_sockets_max
+        self.slots = []
+        self.slots_max = slots_max
 
-        # Title modifiers
+        # Rarity / Minigame Effect
+        self.apply_effect(ProportionalEffect(modifier, modifier, modifier))
+
+        # Title effects
         titles_data = load_jsonc(os.path.join(".", "data", "titles.jsonc"))
         for title in titles:
             if title not in titles_data:
@@ -45,38 +58,29 @@ class Part:
 
                 scaling = effects.get("scaling")
                 if scaling == "proportional":
-                    stitches *= effects.get("stitch", 1)
-                    nerves *= effects.get("nerve", 1)
-                    bones *= effects.get("bone", 1)
+                    self.apply_effect(
+                        ProportionalEffect(
+                            effects.get("health", 1),
+                            effects.get("defense", 1),
+                            effects.get("attack", 1),
+                        )
+                    )
                 elif scaling == "static":
-                    stitches += effects.get("stitch", 0)
-                    nerves += effects.get("nerve", 0)
-                    bones += effects.get("bone", 0)
+                    self.apply_effect(
+                        StaticEffect(
+                            effects.get("health", 0),
+                            effects.get("defense", 0),
+                            effects.get("attack", 0),
+                        )
+                    )
                 else:
                     raise RuntimeError(f'Invalid scaling metric "{scaling}"')
 
-        stitches = round(stitches)
-        nerves = round(nerves)
-        bones = round(bones)
-
-        self.stitches = stitches
-        self.stitches_max = stitches
-        self.nerves = nerves
-        self.nerves_max = nerves
-        self.bones = bones
-        self.bones_max = bones
-        self.female_sockets: list[Part] = []
-        self.female_sockets_max = female_sockets_max
-        self.male_sockets: list[Part] = []
-        self.male_sockets_max = male_sockets_max
-        self.slots = []
-        self.slots_max = slots_max
-
     def __repr__(self):
         return f"""Part(
-    stitches={self.stitches},
-    nerves={self.nerves},
-    bones={self.bones},
+    health={self.health},
+    defense={self.defense},
+    attack={self.attack},
     female_sockets_max={self.female_sockets_max},
     female_sockets_used={len(self.female_sockets)},
     male_sockets_max={self.male_sockets_max},
@@ -84,6 +88,11 @@ class Part:
     slots_max={self.slots_max},
     slots_used={len(self.slots)}
 )"""
+
+    def apply_effect(self, effect: Effect):
+        self.health, self.defense, self.attack = effect.apply(
+            self.health, self.defense, self.attack
+        )
 
     @classmethod
     def factory_natural(cls, name: str, titles: tuple[str] = tuple()):
@@ -94,23 +103,42 @@ class Part:
         if part_data is None:
             raise ValueError(f'Part "{name}" not found.')
 
-        base_stats = part_data.get("baseStats")
-        stitches = base_stats.get("stitch")
-        nerves = base_stats.get("nerve")
-        bones = base_stats.get("bone")
-
+        cost = part_data.get("cost")
         sockets = part_data.get("sockets")
         female_sockets_max = sockets.get("female")
         male_sockets_max = sockets.get("male")
-
         slots_max = part_data.get("slots")
 
-        modifier = random.uniform(0.5, 2)
+        return Part(
+            health=cost // 3,
+            defense=cost // 3,
+            attack=cost // 3,
+            female_sockets_max=female_sockets_max,
+            male_sockets_max=male_sockets_max,
+            slots_max=slots_max,
+            modifier=random.uniform(0.5, 2),
+            titles=titles,
+        )
+
+    @classmethod
+    def factory_player(cls, name: str, modifier: float, titles: tuple[str] = tuple()):
+
+        parts_data = load_jsonc(os.path.join(".", "data", "parts.jsonc"))
+
+        part_data = parts_data.get(name)
+        if part_data is None:
+            raise ValueError(f'Part "{name}" not found.')
+
+        cost = part_data.get("cost")
+        sockets = part_data.get("sockets")
+        female_sockets_max = sockets.get("female")
+        male_sockets_max = sockets.get("male")
+        slots_max = part_data.get("slots")
 
         return Part(
-            stitches=stitches,
-            nerves=nerves,
-            bones=bones,
+            health=cost // 3,
+            defense=cost // 3,
+            attack=cost // 3,
             female_sockets_max=female_sockets_max,
             male_sockets_max=male_sockets_max,
             slots_max=slots_max,
